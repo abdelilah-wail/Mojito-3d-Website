343e17a7361f14b0f8abab25a075ce6be74714b3
Macbook <macbook@MacBook-Pro-de-Macbook.local>
2025-12-14 22:49:37 +0100

    implement Here section

--- Mojito/src/App.jsx
+++ Mojito/src/App.jsx
@@ -1,13 +1,19 @@
-import gsap from "gsap"
-import { ScrollTrigger , SplitText } from "gsap/all"
+import { gsap } from "gsap"
+import { ScrollTrigger, SplitText } from 'gsap/all'
+import Navbar from './components/Navbar'
+import Hero from "./components/Hero"
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const App = () => {
   return (
-    <div className="flex-center h-[100vh] ">
-        <h1 className="text-6xl font-bold">Mojito 3D Website</h1>
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black">
+
+      </div>
+    </main>
   )
 }
 
